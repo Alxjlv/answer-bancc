fa6ed9e62a31fec1e4ce75f50439523e18dc40be
Alxjlv <alxjlv@gmail.com>
2019-07-06 10:41:44 +1200

    Initial commit - project files added

---
+++ build/precache-manifest.d1f0afb4832fffde8ab3b55e9dcfbd85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93afa66c6351981ffb803c0e42a0e690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cf807f9b5afa33d98f2",
+    "url": "/static/css/2.336ba08f.chunk.css"
+  },
+  {
+    "revision": "9190b8b11f0ce90d1cb0",
+    "url": "/static/css/main.82140b7d.chunk.css"
+  },
+  {
+    "revision": "4cf807f9b5afa33d98f2",
+    "url": "/static/js/2.f8c292cc.chunk.js"
+  },
+  {
+    "revision": "9190b8b11f0ce90d1cb0",
+    "url": "/static/js/main.6450c684.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
